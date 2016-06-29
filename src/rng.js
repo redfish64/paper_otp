9fc95370400ed79432e87aee11be1089ae0d0e7c
Tim Engler <engler@gmail.com>
2016-06-29 18:01:44 +0800

    Pretty much works now

--- src/rng.js
+++ src/rng.js
@@ -1,36 +1,50 @@
+
 var redfish64 = { };
 
 //stolen from bitaddress.org TODO 2 give attrition
+
+
+//the sjcl mouse collector is really weird. It will allow a non-moving static mouse to be considered
+//as "random" movement
 redfish64.seeder = {
-    init: (function () {
-    })(),
-
-    // number of mouse movements to wait for
-    seedLimit: (function () {
-	return 200 + Math.floor(Math.random()*12);
-    })(),
-
-    seedCount: 0, // counter
     lastInputTime: new Date().getTime(),
     seedPoints: [],
-    seedData: [],
-    isStillSeeding: true,
-
-    // seed function exists to wait for mouse movement to add more entropy before generating an address
-    seed: function (evt) {
-	if (!evt) var evt = window.event;
-	var timeStamp = new Date().getTime();
-	// seeding is over now we generate and display the address
-	if (redfish64.seeder.seedCount == redfish64.seeder.seedLimit) {
-	    redfish64.seeder.seedCount++;
-	    redfish64.seeder.seedingOver();
+    isStillSeeding: false,
+ 
+    start: function() {
+	if (window.addEventListener) {
+	    window.addEventListener("mousemove", redfish64.seeder._seed, false);
+	} else if (document.attachEvent) {
+	    document.attachEvent("onmousemove", redfish64.seeder._seed);
+	} else {
+	    throw new sjcl.exception.bug("can't attach event");
 	}
-	// seed mouse position X and Y when mouse movements are greater than 40ms apart.
-	else if ((redfish64.seeder.seedCount < redfish64.seeder.seedLimit) && evt && (timeStamp - redfish64.seeder.lastInputTime) > 40) {
-	    redfish64.seeder.seedData.push([evt.clientX, evt.clientY, +new Date]);
-	    redfish64.seeder.showPoint(evt.clientX, evt.clientY);
-	    redfish64.seeder.seedCount++;
-	    redfish64.seeder.lastInputTime = new Date().getTime();
+  //	console.log("stART!!");
+    },
+
+    onfinish: function(f) {
+	redfish64.seeder.onfinish = f;
+    },
+
+    _seed: function (evt) {
+	if(sjcl.random.isReady())
+	{
+	    redfish64.seeder.seedingOver();
+	    return;
+	}
+	
+	timeStamp = new Date().getTime();
+	if (!evt) var evt = window.event;
+
+	var x = evt.x || evt.clientX || evt.offsetX || 0;
+	var y = evt.y || evt.clientY || evt.offsetY || 0;
+	
+	if (evt && (timeStamp - redfish64.seeder.lastInputTime) > 40)
+	{
+	    //console.log(x,y)
+	    sjcl.random.addEntropy([x, y], 2, "mouse");
+	    redfish64.seeder.showPoint(redfish64.seeder.lastX= x, redfish64.seeder.lastY=y);
+	    redfish64.seeder.lastInputTime = timeStamp;
 	}
     },
 
@@ -51,9 +65,12 @@ redfish64.seeder = {
     },
 
     seedingOver: function () {
-	redfish64.seeder.isStillSeeding = false;
+	if (window.removeEventListener) {
+	    window.removeEventListener("mousemove", redfish64.seeder._seed, false);
+	} else if (document.detachEvent) {
+	    document.detachEvent("onmousemove", redfish64.seeder._seed);
+	}
 	redfish64.seeder.removePoints();
-
-	Math.seedrandom(seedData, { entropy: true });
+	redfish64.seeder.onfinish();
     }
 };
